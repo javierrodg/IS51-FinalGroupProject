5e4c631ad3bd54e5e16019be74a272496e60f96d
Javier Rodriguez <javiersmac@Javierss-Mac-mini.local>
2021-05-09 00:07:52 -0700

    finish structured English

---
+++ finalproject.py
@@ -0,0 +1,28 @@
+"""
+Structured English
+This program has four useful functions that a user can use.
+The first function ask the user to input a number to calculate to celsius 
+We ask the user for the number, then take the number and insert to the formula
+Then print out the result and concatenate with text.
+
+The second function estimates the time of arrival entered by the user.
+Ask the user for the distance travelled and store it in a value 
+Then ask the user for the speed and store the number in a variable.
+Then take distance and divide it by speed * 60 to calculate the formula
+Print time arrived in minutes.
+
+The third function sorts the numbers entered the user from least to greatest
+Create an empty list to store the numbers entered by the user
+Create a loop that only takes four values from the user.
+Continues to ask user until four values are entered
+Print the list of numbers sorted from least to greatest
+
+The fourth function prints get the greatest common factor of two numbers
+Ask the user for the first number and store the input 
+Ask the user for the second number and store the input
+Create a while loop that takes the first number and use modulo on the second number 
+Print the greatest common number between the two numbers.
+
+Last function called main invokes all the functions
+
+"""
